13da485f7e9b2387cc4de859e0f7d5d9aaf7b2f2
redhead <yana.sitkowecz@yandex.ru>
2022-02-10 08:19:31 +0300

    mountain arrays

--- Task2/main.cpp
+++ Task2/main.cpp
@@ -3,15 +3,15 @@
 #include <iterator>
 #include <algorithm>
 #include <sstream>
-bool is_mountain(std::vector <float>& v)
+bool is_mountain(std::vector <int>& v)
 {
     if (v.size() >= 3) {
-        int size = v.size() - 1;
+        int size = v.size() - 1; int count{};
         for (int i{}; i < size; ++i) {
-            if (v[i] / v[i + 1] < 1)continue;
-            else {
+            if (v[i] < v[i + 1]) {++count;continue;}
+            else if(count!=0){
                 for (; i < size; ++i) {
-                    if (v[i] / v[i + 1] > 1) continue;
+                    if (v[i] > v[i + 1]) continue;
                     else return false;
                 }
                 return true;
@@ -22,11 +22,11 @@ bool is_mountain(std::vector <float>& v)
 }
 int main()
 {
-    std::vector <float> v;
+    std::vector <int> v;
     std::string line; std::cout << "Input:";
     getline(std::cin, line);
     std::istringstream l(line);
-    copy(std::istream_iterator <float>(l), std::istream_iterator<float>(), back_inserter(v));
+    copy(std::istream_iterator <int>(l), std::istream_iterator<int>(), back_inserter(v));
     std::cout << '\n';
     (is_mountain(v)) ? std::cout << "Output:" << "true" << '\n' : std::cout << "Output:" << "false" << '\n';
 }
